e87e1b722842f2cd70c59b37737eadf23e84fc35
lily <lily.you@student.tdsb.on.ca>
2025-02-03 14:33:14 -0500

    Update PP1_1.java

--- PP1_1.java
+++ PP1_1.java
@@ -1,7 +1,7 @@
 public class PP1_1 {
 
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+		System.out.println("Hello World!");
 		
 	}
 
