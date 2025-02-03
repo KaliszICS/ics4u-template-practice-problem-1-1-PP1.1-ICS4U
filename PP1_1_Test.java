42fae40e5b5ba8bc41f94a746e18f3dc24ec2bb1
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-03 15:22:01 -0500

    Update PP1_1_Test.java

--- PP1_1_Test.java
+++ PP1_1_Test.java
@@ -12,17 +12,6 @@ public class PP1_1_Test {
    @Test
    public void testHelloWorld()
    {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
-
-     // action
-     Hello.main(null);
-
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
-
-     // undo the binding in System
-     System.setOut(originalOut);
+      assertEquals(PP1_1.func(1), 1);
    }
 }
